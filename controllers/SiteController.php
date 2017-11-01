9e6582f84681c044a5f0720515dec7eaa5276a37
Raul Alanis <rulosc07@gmail.com>
2017-11-01 12:18:30 -0600

    otro cambio

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -50,7 +50,7 @@ class SiteController extends Controller {
 	/**
 	 * @inheritdoc
 	 */
-	public function actions() {
+	public function actions(){
 		return [
 				'error' => [
 						'class' => 'yii\web\ErrorAction'
